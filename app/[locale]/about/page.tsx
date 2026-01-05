f3aa358ed89222f134f2273c6c0f59e32c9b69ad
Lucas Pazzim <lpazzim@gmail.com>
2026-01-05 15:08:37 -0300

    last updates

--- app/[locale]/about/page.tsx
+++ app/[locale]/about/page.tsx
@@ -1,7 +1,17 @@
+import Image from "next/image";
+
 export default function AboutPage(){
     return(
-           <div className="bg-black font-sans">   
-      <h1 className="text-white text-4xl font-bold text-center pt-20">About Page</h1>
-    </div>
+        <section aria-labelledby="about-heading">
+            <div className="container w-full mx-auto">
+              <Image
+                src="/images/lucas-pazzim-about.png"
+                alt="About Banner"
+                width={300}
+                height={200}
+                priority
+              />
+            </div>
+        </section>
     )
 }
